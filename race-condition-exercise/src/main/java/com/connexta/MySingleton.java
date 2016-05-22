2e12abf36a10ecbb72dc8cb9d786d9213f7f6a5e
Aaron Hoffer <github@hoffer.org>
2016-05-21 22:08:45 -0700

    Cleaned up the race conditions exercise.

--- race-condition-exercise/src/main/java/com/connexta/MySingleton.java
+++ race-condition-exercise/src/main/java/com/connexta/MySingleton.java
@@ -1,14 +1,5 @@
 /**
- * INSTRUCTIONS
- * <p>
- * The singleton pattern is a design pattern that restricts the instantiation of a class to one
- * object. This is useful when exactly one object is needed to coordinate actions across the
- * system [Wikipedia].
- * <p>
- * This goal of this exercise is to modify this class so that it pass the automated unit tests.
- * The unit tests include multiple threads trying to get the instance. By default the test
- * fail because multiple threads are able to create difference instances, when only one should
- * be created.
+ * See the README.md file for instructions.
  */
 
 package com.connexta;
